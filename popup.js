004fb0088cc7a97e5ae13abdd6f5a3bd8de08ee1
Maxim Leyzerovich <service@duqe.com>
2014-09-23 13:03:37 -0400

    Clearing out for this branch.

--- popup.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-   $('#start-btn').on('click', function(){
-	    chrome.runtime.sendMessage({started: true});
-	});
-
-   $('#stop-btn').on('click', function(){
-   		chrome.runtime.sendMessage({started: false});
-   })
-});
