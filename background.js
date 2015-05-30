2b41871e7d6e38300e01e2f9c754b6ec47481718
Ramsay Lanier <ramsay@nclud.com>
2015-05-30 09:59:22 -0400

    init

--- background.js
+++ background.js
@@ -1,175 +1,94 @@
 var started = false,
+	username, deckURL, tabURL,
 	currentTab = 0,
 	pages = [];
 
 
+// When the extension is installed or upgraded ...
+chrome.runtime.onInstalled.addListener(function() {
+	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+		chrome.declarativeContent.onPageChanged.addRules([
+			{
+		        conditions: [
+		          new chrome.declarativeContent.PageStateMatcher({
+		            pageUrl: { urlContains: 'slides.com' },
+		          })
+		        ],
+				actions: [ 
+					new chrome.declarativeContent.ShowPageAction()
+				]
+			}
+		]);
+	});
+});
+
 //listen for popup click to start extension
 chrome.runtime.onMessage.addListener( function(request, sender, response){
-	started = request.started;
+	console.log(request);
 
-	if (started == true)
-		startReactionaly();
-	else if (started == false)
-		endReactionaly();
+	if (request.started){
+		started = request.started;
+		username = request.username;
+		deckURL = request.deckURL;
+
+		start();
+	}
+
+	if (request.stop){
+		stop();
+	}
+
+	response({
+		started: started
+	})
 });
 
 //gets current tab and injects relevant CSS and JS into the tab
-function startReactionaly() {
+function start() {
+	console.log('start');
 	pages = [];
 	chrome.tabs.query({
 		active: true,
 		currentWindow: true
 	}, function(result){
-
 		currentTab = result[0].id;
+		tabURL = result[0].url;
 
-		chrome.tabs.insertCSS(currentTab, {
-			file : "heatmap.css"
-		});
-
-		chrome.tabs.executeScript(currentTab, {file: "jquery-1.11.1.min.js"}, function() {
-			chrome.tabs.executeScript(currentTab, {file: "heatmap.min.js" });
-			chrome.tabs.executeScript(currentTab, {file: "html2canvas.js"});
-		    chrome.tabs.executeScript(currentTab, {file: "reactional.js" });
-		});
+		updateQuotes();
 	})
 }
 
-//ends extension by creating a new window, pulling all stored pages in pages array, and appending them to storage.html file which gets rendered in new window
-function endReactionaly() {
+function stop(){
+	started = false;
+	chrome.pageAction.setIcon({
+        path: '/images/icon-38.png',
+        tabId: currentTab
+    });
+}
 
-	//push current page into pages array
-	chrome.storage.local.get('page', function(result){
-		var page = JSON.parse(result.page);
-		var document;
+function updateQuotes(){
+	console.log('updateQuotes');
 
-		pages.push(page);
+	chrome.pageAction.setIcon({
+        path: '/images/active-icon.png',
+        tabId: currentTab
+    });
 
-		var views = chrome.extension.getViews();
-		var storageCreated = views.some(function(view){
-			return endsWith(view.location.href, "/storage.html");
-		});
-
-		if (!storageCreated){
-			chrome.tabs.create({
-				url: "storage.html",
-				active: true
-			}, function(){
-				views = chrome.extension.getViews();
-				views.forEach(function(view){
-					if (endsWith(view.location.href, "/storage.html")){
-						document = view.document;
-						renderPages(document, pages);
-					}
-				});
-
-				resetPages();
-			});
-		} else {
-			views.forEach(function(view){
-				if (endsWith(view.location.href, "/storage.html")){
-					document = view.document;
-					clearDocument(document);
-					renderPages(document, pages);
-				}
-			});
-
-			//reset pages
-			resetPages();
-		}
+	$.ajax({
+	  method: "post",
+	  url: "http://localhost:3000/methods/updateQuotesFromExtension",
+	  data: JSON.stringify([tabURL, username, deckURL]),
+	  contentType: "application/json",
+	  success: function (data) {
+	  	console.log(data);	  	
+	  }
 	});
 }
 
 chrome.tabs.onUpdated.addListener(function(tabId, changedInfo, tab){
 	if (started == true && changedInfo.url && tabId == currentTab){
-
-		chrome.tabs.insertCSS(currentTab, {
-			file : "heatmap.css"
-		});
-
-		chrome.tabs.executeScript(currentTab, {file: "jquery-1.11.1.min.js"}, function() {
-			chrome.tabs.executeScript(currentTab, {file: "heatmap.min.js" });
-			chrome.tabs.executeScript(currentTab, {file: "html2canvas.js"});
-		    chrome.tabs.executeScript(currentTab, {file: "reactional.js" });
-		});
-
-		chrome.storage.local.get('page', function(result){
-			var page = JSON.parse(result.page);
-			pages.push(page);
-		});
+		tabURL = tab.url
+		updateQuotes();
 	}
 });
-
-function renderPages(document, pages){
-	pages.forEach(function(page, index){
-
-
-		//create screenshot container and add to document
-		var container = document.createElement("div");
-		container.setAttribute("class", "screenshot-container");
-		document.body.appendChild(container);
-
-
-		//create new canvas an append to container
-		var screenShotCanvas = document.createElement("canvas");
-		screenShotCanvas.setAttribute("class", "canvas screenshot-canvas");
-		screenShotCanvas.setAttribute("id", "screenshot-canvas-" + (index + 1));
-		container.appendChild(screenShotCanvas);
-
-		//create new context for canvas
-		var screenShotCtx = screenShotCanvas.getContext("2d");
-
-		var screenShot = new Image;
-		screenShot.src = page.initialShot;
-
-		screenShotCanvas.width = screenShot.width;
-		screenShotCanvas.height = screenShot.width;
-
-		//draw screenshot onto canvas
-		screenShot.onload = function(){
-			var imgWidth = this.width;
-			var imgHeight = this.height;
-			screenShotCtx.drawImage(screenShot, 0, 0, imgWidth, imgHeight);
-		}
-
-
-		//create new heatmap canvas an append to container
-		var heatmapCanvas = document.createElement("canvas");
-		heatmapCanvas.setAttribute("class", "canvas heatmap-canvas");
-		heatmapCanvas.setAttribute("id", "heatmap-canvas-" + (index + 1));
-		container.appendChild(heatmapCanvas);
-
-		//create new context for canvas
-		var heatmapCtx = heatmapCanvas.getContext("2d");
-
-		var heatmap = new Image;
-		heatmap.src = page.heatmap;
-
-		heatmapCanvas.width = heatmap.width;
-		heatmapCanvas.height = heatmap.width;
-
-		//draw heatmap onto canvas
-		heatmap.onload = function(){
-			var imgWidth = this.width;
-			var imgHeight = this.height;
-			heatmapCtx.drawImage(heatmap, 0, 0, imgWidth, imgHeight);
-		}
-	})
-}
-
-function resetPages(){
-	pages = [];
-	chrome.storage.local.remove('page');
-}
-
-function clearDocument(document){
-	var containers = document.getElementsByClassName('screenshot-container');
-	while (containers.length > 0){
-		containers[0].remove();
-	}
-}
-
-function endsWith(str, suffix) {
-    return str.indexOf(suffix, str.length - suffix.length) !== -1;
-}
 	
--- popup.js
+++ popup.js
@@ -1,9 +1,63 @@
 $(function() {
-   $('#start-btn').on('click', function(){
-	    chrome.runtime.sendMessage({started: true});
+
+	chrome.runtime.sendMessage({}, function(response){
+		console.log(response.started);
+		
+		if (response.started){
+			hideForm();
+		}
 	});
 
-   $('#stop-btn').on('click', function(){
-   		chrome.runtime.sendMessage({started: false});
+	var errors = $('.errors');
+	var error;
+
+   $('.pq-start-form').on('submit', function(e){
+   		e.preventDefault();
+   		var username = $('.pq-username-field').val();
+   		var deckURL = $('.pq-deck-url-field').val();
+   		var password = $('.pq-password-field').val();
+
+   		if (!username){
+   			throwError('Please enter a username');
+   		} else if (!password){
+   			throwError('Please enter a password');
+   		} else if (!deckURL){
+   			throwError('Please enter the URL of the deck you want to sync.');
+   		} else {
+   			$.ajax({
+				method: "post",
+				url: "http://localhost:3000/methods/loginFromExtension",
+				data: JSON.stringify([username, password]),
+				contentType: "application/json",
+				success: function (data) {
+					console.log(data);
+					if (data === true){
+						$('.errors').html('');
+						chrome.runtime.sendMessage({started: true, username: username,deckURL: deckURL});
+						window.close();
+					} else{
+						throwError(data);
+					}
+				},
+				error: function(error){
+					throwError(error)
+				}
+			});
+   		}
+   });
+
+   $('.stop-btn').on('click', function(){
+   		chrome.runtime.sendMessage({stop: true});
+   		window.close();
    })
 });
+
+function throwError(error){
+	$('.errors').html('<p class="error">' + error + '</p>');
+	return false;
+}
+
+function hideForm(){
+	$('.pq-start-form').addClass('hidden');
+	$('.stop-btn').removeClass('hidden');
+}
